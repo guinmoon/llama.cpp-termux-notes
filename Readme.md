b9227fd6d85fd8478b1507575015988566ed89bb
guinmoon <guinmoon@gmail.com>
2025-09-10 20:12:58 +0300

    fix build

--- Readme.md
+++ Readme.md
@@ -1,5 +1,5 @@
 # Build and run llama.cpp in Termux
 
 ```bash
-bash <(curl -s https://github.com/guinmoon/llama.cpp-termux-notes/raw/refs/heads/main/build_full.sh)
+bash <(curl -s https://raw.githubusercontent.com/guinmoon/llama.cpp-termux-notes/refs/heads/main/full_build.sh)
 ```
--- full_build.sh
+++ full_build.sh
@@ -2,12 +2,13 @@
 echo "Cloning repo and llama.cpp"
 git clone --recurse-submodules https://github.com/guinmoon/llama.cpp-termux-notes
 cd llama.cpp-termux-notes
+chmod +x *.sh
 ./install_req.sh
 cd llama.cpp
-./build_CPU.sh
-./build_OpenBLAS.sh
-./build_OpenBLIS.sh
-./build_KleidiAI.sh
-./build_OpenCL.sh
-./build_Vulkan.sh
-./build_Vulkan_OpenCL.sh
+../build_CPU.sh
+../build_OpenBLAS.sh
+../build_OpenBLIS.sh
+../build_KleidiAI.sh
+../build_OpenCL.sh
+../build_Vulkan.sh
+../build_Vulkan_OpenCL.sh
